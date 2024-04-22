6f8f110a500724034d7c5d6d46cb480071372428
Mostafa Taha <mostafataha011188@gmail.com>
2024-04-22 22:44:31 +0200

    finish about us create and show

--- app/Http/Controllers/Api/AboutUsController.php
+++ app/Http/Controllers/Api/AboutUsController.php
@@ -9,15 +9,15 @@ use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
-    public function mainPage(Request $request)
+    public function create(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'title' => ['required', 'string'],
             'our_goals' => ['required', 'array'],
             'students_number' => ['required', 'numeric', 'min:1'],
             'graduated_students' => ['required', 'numeric', 'min:1'],
             'question' => ['nullable', 'max:255'],
-            'answer' => ['required', 'array', 'max:255'],
+            'answers' => ['required', 'array', 'max:255'],
             'constructed_at' => ['required', 'string'],
             'organizational_char_image' => ['nullable', 'image'],
             'institute_dean_name' => ['required', 'string'],
@@ -27,17 +27,28 @@ class AboutUsController extends Controller
             'chairman_of_board_image' => ['nullable', 'image'],
             'chairman_of_board_word' => ['nullable', 'string']
         ]);
+        $data['our_goals'] = implode(',', $request->post('our_goals'));
+        $data['answers'] = implode(',', $request->post('answers'));
+        $data['institute_dean_image'] = $this->uploadedImage($request, 'institute_dean_image');
+        $data['chairman_of_board_image'] = $this->uploadedImage($request, 'chairman_of_board_image');
+        $data['organizational_char_image'] = $this->uploadedImage($request, 'organizational_char_image');
 
-        $request['institute_dean_image'] = $this->uploadedImage($request, 'institute_dean_image');
-        $request['chairman_of_board_image'] = $this->uploadedImage($request, 'chairman_of_board_image');
-
-        AboutUs::create($request->all());
+        AboutUs::create($data);
 
         return SendResponse::sendResponse(201, 'Data Created Successfully', []);
     }
 
+    public function show()
+    {
+        $data = AboutUs::all();
+        if (count($data) > 0) {
+            return SendResponse::sendResponse(200, 'Data Retrieved Successfully', $data);
+        }
+        return SendResponse::sendResponse(200, 'No Data To Retrieved', []);
+    }
 
-    protected function uploadedImage(Request $request, $fileName)
+
+    public function uploadedImage(Request $request, $fileName)
     {
         // if request hasn't it will make this method return null and if not it will return the path
         if (!$request->hasFile($fileName)) {
