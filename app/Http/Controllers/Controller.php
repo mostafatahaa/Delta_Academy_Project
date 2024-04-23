a640217bcd88b433071ace34047441ab890aafe7
Mostafa Taha <mostafataha011188@gmail.com>
2024-04-23 22:12:51 +0200

    add upload image function to main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    public function uploadedImage(Request $request, $fileName)
+    {
+        // if request hasn't it will make this method return null and if not it will return the path
+        if (!$request->hasFile($fileName)) {
+            return;
+        }
+
+        $file = $request->file($fileName); // return uploadedFile object
+        $path =  $file->store('uploads/images', [
+            'disk' => 'public'
+        ]); // or i can put key and value ('disk' => 'public')
+        return $path;
+    }
 }
